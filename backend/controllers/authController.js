05dfd1847b34a52277556676e30d4fdfecdabc6b
Raj Aryan <rajaryan26940@gmail.com>
2026-07-01 23:46:00 +0530

    Complete Login API with bcrypt password verification

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,5 +29,31 @@ const user = new User({
   });
 }
 };
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+  return res.status(404).json({
+    message: "User not found",
+  });
+}
+const isMatch = await bcrypt.compare(password, user.password);
+if (!isMatch) {
+  return res.status(401).json({
+    message: "Invalid Password",
+  });
+}
+res.status(200).json({
+  message: "Login Successful",
+});
+  } catch (error) {
+    console.log(error)
 
-module.exports = { registerUser };
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser };
