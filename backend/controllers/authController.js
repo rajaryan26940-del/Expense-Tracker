93055d27a8f4a509010f9cb1327089c1b47d26fe
Raj Aryan <rajaryan26940@gmail.com>
2026-07-01 19:38:27 +0530

    Complete Register API with password hashing

---
+++ backend/controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const registerUser = async (req, res) => {
+  try {
+   const { name, email, password } = req.body;
+
+const hashedPassword = await bcrypt.hash(password, 10);
+
+const user = new User({
+  name,
+  email,
+  password: hashedPassword,
+});
+
+    await user.save();
+
+    res.send("User Registered Successfully");
+  } catch (error) {
+  console.log(error);
+
+  if (error.code === 11000) {
+    return res.status(400).json({
+      message: "Email already exists",
+    });
+  }
+
+  res.status(500).json({
+    message: "Internal Server Error",
+  });
+}
+};
+
+module.exports = { registerUser };
--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ require("dotenv").config();
 console.log("PORT:", process.env.PORT);
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
